418aed410a1a87578c71fdf6a7a664263ce38f70
docedson <docedson@gmail.com>
2018-12-28 11:16:55 -0600

    Ive added code comments

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
+/*This name space reresents the file folder that
+the class is in. */
 namespace classwork
 {
+    //Class is a blueprint of an object
     class Program
-    {
+    {/*This is a return type having only one item
+    and a method name with an array. */
         static void Main(string[] args)
-        {
+        {//This is a class and its method with a parameter
             Console.WriteLine("Hello World!");
+            //The following brackets close off everything within themselves
         }
     }
 }
